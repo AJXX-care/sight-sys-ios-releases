38acd53a9ef63fc733491a26597c8a01505b474a
guohui <guohuiofficial@gmail.com>
2024-07-30 00:39:48 +0800

    [chore]: v1.0.0-v1-SNAPSHOT-20240730

---
+++ releases/Core/SightSysSDK.xcframework/ios-arm64/SightSysSDK.framework/PrivateHeaders/SightSysAdapterAutoInjection.h
@@ -0,0 +1,18 @@
+//
+//  SightSysAdapterAutoInjection.h
+//  SightSysSDK
+//
+//  Created by Hui Guo on 2024/7/29.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SightSysAdapterAutoInjection : NSObject
+
++ (void)registerAll;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ releases/TuYa/SightSysAdapterTuYa.xcframework/ios-arm64/SightSysAdapterTuYa.framework/Headers/SightSysAdapterTuYa.h
@@ -0,0 +1,18 @@
+//
+//  SightSysAdapterTuYa.h
+//  SightSysAdapterTuYa
+//
+//  Created by Hui Guo on 2024/7/29.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SightSysAdapterTuYa.
+FOUNDATION_EXPORT double SightSysAdapterTuYaVersionNumber;
+
+//! Project version string for SightSysAdapterTuYa.
+FOUNDATION_EXPORT const unsigned char SightSysAdapterTuYaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SightSysAdapterTuYa/PublicHeader.h>
+
+
